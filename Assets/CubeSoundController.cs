252a425649b5f489b0168700bed74e09338daf27
Saepon <s.haisa.0426@gmail.com>
2020-07-01 18:13:06 +0900

    GoForwardUnityChan
    宜しくお願い致します。

---
+++ Assets/CubeSoundController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSoundController : MonoBehaviour
+{
+    AudioSource audioSource;
+
+      public AudioClip BlockSE;
+ 
+// 地面の位置
+private float groundLevel = -3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    audioSource = GetComponent<AudioSource>();
+    }
+   //他のキューブと接触したら
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.tag == "Cube")
+            {
+                audioSource.PlayOneShot(BlockSE);
+            }
+
+            else if (collision.gameObject.tag == "ground")
+            {
+                audioSource.PlayOneShot(BlockSE);
+
+
+
+
+            }
+        }
+    // Update is called once per frame
+    void Update()
+    {
+
+     
+   
+        }
+    }
+
